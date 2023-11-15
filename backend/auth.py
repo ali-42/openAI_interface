16ba3e08552c3a29014e9c9eb80dd3c366e96b51
ali-42 <li.arthur45@gmail.com>
2023-11-15 18:53:41 +0100

    backup

--- backend/auth.py
+++ backend/auth.py
@@ -43,7 +43,7 @@ def authenticat_user(username: str, password: str, db: db_dependancy):
     user = db.query(Users).filter(Users.username == username).first()
     if not user:
         return False
-    if not bcrypt_context.verify(password, str(user.password)):
+    if not bcrypt_context.verify(password, user.password):
         return False
     return user
 
@@ -56,8 +56,8 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
 async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get('sub')
-        user_id = payload.get('id')
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
         if username is None or user_id is None:
             raise HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED,
